eecf116da801c5c6f607e285bad4becb38f808f6
Morgan Hill <a@morganrhill.com>
2024-08-07 18:13:41 +0200

    Make the target take a bit longer

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,17 @@ mod tests {
 
     #[tokio::test]
     async fn simple_target_test() {
-        assert_eq!(simple_target(0, b"Hello").await,b'H');
-        assert_ne!(simple_target(0, b"Hello").await,b'h');
-        assert_eq!(simple_target(3, b"Hello").await,b'l')
+        assert_eq!(simple_target(0, b"Hello").await,4) 
     }
 }
 
-
 pub async fn simple_target(i: usize, s: &[u8]) -> u8 {
-    s[i]
+   if i % 42 == 23 && s.len() > 500 {
+    for c in s {
+        if *c as usize * i == 3000 {
+            panic!()
+        }
+    }
+   } 
+   4
 }
