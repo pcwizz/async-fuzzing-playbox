0662c6df5cc562db4d4f5aae6940a9d1cbced7b5
Morgan Hill <a@morganrhill.com>
2024-08-10 18:14:34 +0200

    Add reading target

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use core::time;
+
+use tokio::io::AsyncReadExt;
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -5,6 +9,23 @@ mod tests {
     #[tokio::test]
     async fn simple_target_test() {
         assert_eq!(simple_target(0, b"Hello").await, 4)
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn reading_target_test_bug() {
+        let reader = tokio_test::io::Builder::new()
+            .read(&[0, 6, 2, 1, 45, 65, 3, 54, 5, 9, 0, 6, 2, 1, 45, 65, 3])
+            .build();
+        reading_target(reader).await
+    }
+
+    #[tokio::test]
+    async fn reading_target_test_no_bug() {
+        let reader = tokio_test::io::Builder::new()
+            .read(&[0, 6, 2, 1, 45, 65, 3, 5, 5, 9, 0, 6, 2, 1, 45, 65, 3])
+            .build();
+        reading_target(reader).await
     }
 }
 
@@ -17,4 +38,21 @@ pub async fn simple_target(i: usize, s: &[u8]) -> u8 {
         }
     }
     4
+}
+
+pub async fn reading_target<Reader>(r: Reader)
+where
+    Reader: tokio::io::AsyncRead + Unpin,
+{
+    let mut reader = tokio::io::BufReader::new(r);
+    let mut buffer = [0; 10];
+    loop {
+        let n = reader.read(&mut buffer).await.unwrap();
+        if n == 0 {
+            break;
+        }
+        if n == 7 {
+            assert_ne!(buffer, [0, 6, 2, 1, 45, 65, 3, 54, 5, 9]);
+        }
+    }
 }
